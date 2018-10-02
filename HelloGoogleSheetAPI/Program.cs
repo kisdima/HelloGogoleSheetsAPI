bce4f93132fc72d326e05978d4ab2445af87e93b
Dmitry Kiselev <dkiselev@deloitte.com>
2018-10-02 17:37:13 +0300

    init

---
+++ HelloGoogleSheetAPI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloGoogleSheetAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
